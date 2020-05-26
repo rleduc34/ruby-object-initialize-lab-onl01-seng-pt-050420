e10134db507c12b220918ddcdeb7f86085ac8b1f
Robert Leduc <rleduc34@gmail.com>
2020-05-26 22:24:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,16 @@
 class Dog
-  def initialize (name)
-    @name =  name
-    @breed = breed
+  def initialize (dogs_name, dogs_breed)
+    @name =  dogs_name
+    @breed = dogs_breed
   end
-  def name=(name)
-    @name = name
-    @breed = breed
+  def name=(dogs_name, dogs_breed = "Mutt")
+    @name =  dogs_name
+    @breed = dogs_breed
   end
   def name
     @name
+  end
+  def breed
     @breed
   end
 end
